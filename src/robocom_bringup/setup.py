38e6500477d4e4ecf5b62b1deefd283e1c145962
petershe1by <2399978813@qq.com>
2026-07-06 03:01:00 +0800

    fix: explicit packages list in setup.py for 3 packages not registering in AMENT_PREFIX_PATH

--- src/robocom_bringup/setup.py
+++ src/robocom_bringup/setup.py
@@ -7,7 +7,7 @@ package_name = 'robocom_bringup'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(),
+    packages=['robocom_bringup'],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
--- src/robocom_motion_control/setup.py
+++ src/robocom_motion_control/setup.py
@@ -5,7 +5,7 @@ package_name = 'robocom_motion_control'
 setup(
     name=package_name,
     version='1.0.0',
-    packages=find_packages(),
+    packages=['robocom_motion_control'],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
